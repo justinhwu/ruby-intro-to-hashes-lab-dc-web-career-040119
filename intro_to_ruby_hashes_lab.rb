41f084db83169b97e7601a7ad2d7b201f7ff7905
Justin Wu <justinhwu95@gmail.com>
2019-03-18 21:16:41 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -13,7 +13,8 @@ end
 
 def monopoly_with_second_tier
 	#copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
-
+  {name: "Steven", age: "30", attitude: "confused"},
+          {name: "Nurse", age: "44", attitude: "worried"}
 end
 
 def monopoly_with_third_tier
